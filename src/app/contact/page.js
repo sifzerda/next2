5d1c4599228f1c63467d704548f6a3c48ac1bfcb
Troy <tydamon@hotmail.com>
2024-12-16 21:21:40 +1100

    fixed routing for contact page

---
+++ src/app/contact/page.js
@@ -0,0 +1,10 @@
+// src/app/contact.js
+
+export default function Contact() {
+    return (
+      <div>
+        <h1>Contact</h1>
+        <p>This is the contact page.</p>
+      </div>
+    );
+  }
